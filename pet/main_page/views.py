9858065c970e92cc0d378b78f0217facdfdd8d81
accxracy <gkola22907@gmail.com>
2025-07-14 13:05:26 +0200

    first_page

---
+++ pet/main_page/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
+
+def popular_list(request):
+    products = Product.objects.filter(available=True)[:3]
+    return render(request,
+                  'main_page/index/index.html', {'products': products})
+
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product,
+                                slug=slug,
+                                available=True)
+    return render(request, "main_page/product/detail.html",
+                  {'product': product})
+
