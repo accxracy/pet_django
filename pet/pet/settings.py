b55244a53bce9e3f849190018d3f659690381a4a
accxracy <gkola22907@gmail.com>
2025-07-17 14:35:59 +0200

    cart_finish

--- pet/pet/settings.py
+++ pet/pet/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.for_cart_icon.cart_info'
             ],
         },
     },
