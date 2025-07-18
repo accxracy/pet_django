bdad93c84572fb3af11cc624d92bc6dfa011f9ac
accxracy <gkola22907@gmail.com>
2025-07-19 01:33:39 +0200

    registration and login forms

--- pet/login/urls.py
+++ pet/login/urls.py
@@ -6,7 +6,7 @@ app_name = 'users'
 
 urlpatterns = [
     path('login/', views.login, name='login'),
-    path('register/', views.register, name='register'),
+    path('register/', views.register, name='registration'),
     path('logout/', views.logout, name='logout'),
     path('profile/', views.profile, name='profile'),
 ]
