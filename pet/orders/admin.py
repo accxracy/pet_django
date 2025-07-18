5176ec41d189270a50654435d00266356881037b
accxracy <gkola22907@gmail.com>
2025-07-18 23:38:53 +0200

    Order ORM

--- pet/orders/admin.py
+++ pet/orders/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email', 'city', 
+                    'address', 'postal_code', 'paid', 'time', 'updated']
+    
+    list_filter = ['paid', 'time', 'updated']
+
+    inlines = [OrderItemInline]
--- pet/orders/models.py
+++ pet/orders/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from main_page.models import Product
+from login.models import User
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.SET_DEFAULT,
+                             blank=True, null=True, default=None)
+    
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField()
+    city = models.CharField(max_length=125)
+    address = models.CharField(max_length=275)
+    postal_code = models.CharField(max_length=25)
+    time = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+
+    class Meta:
+        ordering = ['-time']
+        indexes = [
+            models.Index(fields=['-time'])
+        ]
+
+    def __str__(self):
+        return f'order {self.id}'
+    
+    def total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, 
+                              related_name='items',
+                              on_delete=models.CASCADE)
+    
+    product = models.ForeignKey(Product, 
+                                related_name='order',
+                                on_delete=models.CASCADE)
+    
+    price = models.DecimalField(max_digits=10, 
+                                decimal_places=2)
+    
+    quantity = models.PositiveBigIntegerField(default=1)
+
+
+    def __str__(self):
+        return f'{self.id}'
+    
+    def get_cost(self):
+        return self.price * self.quantity
